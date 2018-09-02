08a096838f6ba76382688166069a9dc6ad2aa017
mrzcs <mrzcs@163.com>
2018-09-02 23:17:42 +0800

    commit

--- p1.py
+++ p1.py
@@ -4,3 +4,27 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
+
+style.use('ggplot')
+
+start = dt.datetime(2000,1,1)
+end = dt.datetime(2016,12,31)
+
+#yahoo API doesn't work any more, change it to robinhood or quandl
+#https://pandas-datareader.readthedocs.io/en/latest/remote_data.html#remote-data-quandl
+#df = web.DataReader('TSLA', 'robinhood', start, end)
+df = web.DataReader('WIKI/TSLA', 'quandl', start, end)
+print(df.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ p3.py
@@ -0,0 +1,20 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+
+df = pd.read_csv('TSLA.csv', parse_dates = True, index_col=0)
+
+
+
+
+
+
+
+
+
+
